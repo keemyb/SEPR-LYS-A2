1620305a2b6476d60b0e1a22412aea2db73a2fac
Cameron Miller <cdjm502@york.ac.uk>
2014-11-18 15:42:27 +0000

    Created entry class and package.

---
+++ src/lys/sepr/Application.java
@@ -0,0 +1,13 @@
+package lys.sepr;
+
+public class Application {
+
+	public Application() {
+		//start game coding
+	}
+
+	public static void main(String[] args) {
+		Application app = new Application();
+	}
+
+}
