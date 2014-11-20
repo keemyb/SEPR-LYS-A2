a15891221919433424d952c4e822b06706ac3b7a
Akeem Brown <ab1504@york.ac.uk>
2014-11-20 14:36:44 +0000

    Added Test Stub for Point Class

---
+++ test/lys/sepr/game/world/PointTest.java
@@ -0,0 +1,34 @@
+package lys.sepr.game.world;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testTranslate() throws Exception {
+
+    }
+}
