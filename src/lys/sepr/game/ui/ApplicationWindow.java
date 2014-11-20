d64d8f0c838b5d825c88caa107612def627a5bb2
Cameron Miller <cdjm502@york.ac.uk>
2014-11-20 12:49:47 +0000

    Added a class in package lys.sepr.game.ui

---
+++ src/lys/sepr/game/ui/ApplicationWindow.java
@@ -0,0 +1,11 @@
+package lys.sepr.game.ui;
+
+import javax.swing.JFrame;
+
+public class ApplicationWindow extends JFrame {
+
+	public ApplicationWindow() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
