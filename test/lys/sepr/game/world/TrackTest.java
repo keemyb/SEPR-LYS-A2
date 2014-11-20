2ffc475e8e90851b6d5c2fd362d4feb6c9e96689
Akeem Brown <ab1504@york.ac.uk>
2014-11-20 17:43:09 +0000

    Created Track Test Stub

---
+++ test/lys/sepr/game/world/TrackTest.java
@@ -0,0 +1,13 @@
+package lys.sepr.game.world;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class TrackTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
