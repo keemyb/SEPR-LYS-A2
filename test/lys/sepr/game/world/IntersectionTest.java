f876225f730a977db6de9fae479a3e6bf51da18c
Akeem Brown <ab1504@york.ac.uk>
2014-11-20 18:49:13 +0000

    Created Intersection Test Stub

---
+++ test/lys/sepr/game/world/IntersectionTest.java
@@ -0,0 +1,13 @@
+package lys.sepr.game.world;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class IntersectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
