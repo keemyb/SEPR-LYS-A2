e5bfb5106d439310e8669cbea83a0892f99ccdc1
Akeem Brown <ab1504@york.ac.uk>
2014-11-20 23:56:06 +0000

    Added equals method for Point class

--- src/lys/sepr/game/world/Point.java
+++ src/lys/sepr/game/world/Point.java
@@ -67,4 +67,23 @@ public class Point {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (Float.compare(point.x, x) != 0) return false;
+        if (Float.compare(point.y, y) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
+        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
+        return result;
+    }
 }
