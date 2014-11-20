873cb506d333610bd508765f2fef1e6d9c3506ff
Akeem Brown <ab1504@york.ac.uk>
2014-11-20 19:27:30 +0000

    Added basic intersection and track tests

--- test/lys/sepr/game/world/IntersectionTest.java
+++ test/lys/sepr/game/world/IntersectionTest.java
@@ -2,12 +2,41 @@ package lys.sepr.game.world;
 
 import org.junit.Before;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class IntersectionTest {
 
+    private Track track1;
+    private Track track2;
+
     @Before
     public void setUp() throws Exception {
+        Point startPoint1 = new Point(0,0);
+        Point endPoint1 = new Point(100,100);
 
+        Point startPoint2 = new Point(200,200);
+        Point endPoint2 = new Point(100,100);
+
+        this.track1 = new Track(startPoint1, endPoint1);
+        this.track2 = new Track(startPoint2, endPoint2);
     }
+
+    public void testGetTracks() throws Exception {
+        ArrayList<Track> expectedTracks = new ArrayList<Track>();
+        expectedTracks.add(track1);
+        expectedTracks.add(track2);
+
+        assertEquals(expectedTracks, track1.getIntersection().getTracks());
+    }
+
+    public void testValidNextTracks() throws Exception {
+        ArrayList<Track> expectedTracks = new ArrayList<Track>();
+        expectedTracks.add(track2);
+
+        assertEquals(expectedTracks, track1.getIntersection().getValidNextTracks());
+    }
+
 }
--- test/lys/sepr/game/world/TrackTest.java
+++ test/lys/sepr/game/world/TrackTest.java
@@ -6,8 +6,28 @@ import static org.junit.Assert.*;
 
 public class TrackTest {
 
+    private Track track1;
+    private Track track2;
+
     @Before
     public void setUp() throws Exception {
+        Point startPoint1 = new Point(0,0);
+        Point endPoint1 = new Point(100,100);
 
+        this.track1 = new Track(startPoint1, endPoint1);
     }
+
+    public void testNextTrackSolo() throws Exception {
+        assertEquals(null, track1.nextTrack());
+    }
+
+    public void testNextTrack() throws Exception {
+        Point startPoint2 = new Point(200,200);
+        Point endPoint2 = new Point(100,100);
+
+        this.track2 = new Track(startPoint2, endPoint2);
+
+        assertEquals(track2, track1.nextTrack());
+    }
+    
 }
