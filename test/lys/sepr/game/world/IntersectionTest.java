8f279f87673acb9205f6ac886ef8f8d97c69448a
Akeem Brown <ab1504@york.ac.uk>
2014-11-21 21:47:50 +0000

    Added cross product test

--- test/lys/sepr/game/world/IntersectionTest.java
+++ test/lys/sepr/game/world/IntersectionTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -44,6 +43,16 @@ public class IntersectionTest {
     }
 
     @Test
+    public void testCrossProduct() throws Exception {
+        this.intersection = new Intersection(new Point(100,100), track1, track2);
+        ArrayList<Double> vector1 = track1.getVector(intersection.getPoint());
+        ArrayList<Double> vector2 = track2.getVector(intersection.getPoint());
+
+        assertEquals(0, intersection.crossProduct(vector1, vector2)%180, 0.0d);
+
+    }
+
+    @Test
     public void testValidNextTracksDuo() throws Exception {
         ArrayList<Track> expectedTracks = new ArrayList<Track>();
         expectedTracks.add(track2);
@@ -64,6 +73,21 @@ public class IntersectionTest {
 
         this.intersection = new Intersection(new Point(100,100), track1, track2);
         intersection.addTrack(track3);
+
+//        ArrayList<Double> vector1 = track1.getVector(new Point(100,100));
+//        ArrayList<Double> vector2 = track2.getVector(new Point(100,100));
+//        double angle = intersection.crossProduct(vector1, vector2);
+//        System.out.println(angle);
+//
+//        ArrayList<Double> vector3 = track2.getVector(new Point(100,100));
+//        ArrayList<Double> vector4 = track3.getVector(new Point(100,100));
+//        double angle2 = intersection.crossProduct(vector3, vector4);
+//        System.out.println(angle2);
+//
+//        ArrayList<Double> vector5 = track1.getVector(new Point(100,100));
+//        ArrayList<Double> vector6 = track3.getVector(new Point(100,100));
+//        double angle3 = intersection.crossProduct(vector5, vector6);
+//        System.out.println(angle3);
 
         assertEquals(expectedTracks1, intersection.getValidNextTracks(track1));
         assertEquals(expectedTracks2, intersection.getValidNextTracks(track2));
