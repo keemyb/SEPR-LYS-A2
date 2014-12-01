c60302dca06089f294875c46f3cc3f536a9c8a90
Akeem Brown <ab1504@york.ac.uk>
2014-12-01 14:30:48 +0000

    Created location class

--- src/lys/sepr/game/world/Location.java
+++ src/lys/sepr/game/world/Location.java
@@ -1,5 +1,31 @@
 package lys.sepr.game.world;
 
-public class Location {
+public class Location extends Point{
 
+    private String name;
+
+    Location(double x, double y, String name) {
+        super(x, y);
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Location location = (Location) o;
+
+        if (name != null ? !name.equals(location.name) : location.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
