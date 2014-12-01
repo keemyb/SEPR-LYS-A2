91d1bbcc8e2d0b84ae3910747d394bf10306858b
Akeem Brown <ab1504@york.ac.uk>
2014-12-01 20:15:07 +0000

    Changed routing tests to expect multiple paths
    Also new fastest path test

--- test/lys/sepr/game/world/MapTest.java
+++ test/lys/sepr/game/world/MapTest.java
@@ -13,6 +13,12 @@ public class MapTest {
     private Track track1;
     private Track track2;
     private Track track3;
+    private Track track4;
+    private Track track5;
+    private Track track6;
+    private Track track7;
+    private Location locationOne;
+    private Location locationTwo;
 
     @Before
     public void setUp() throws Exception {
@@ -211,7 +217,7 @@ public class MapTest {
         expectedRoute.add(track1);
         expectedRoute.add(track2);
 
-        assertEquals(expectedRoute, map.getRoute(locationOne, locationTwo));
+        assertTrue(map.getRoutes(locationOne, locationTwo).contains(expectedRoute));
     }
 
     @Test
@@ -224,17 +230,92 @@ public class MapTest {
         map.addTrack(track1);
         map.addTrack(track2);
 
-        assertEquals(null, map.getRoute(locationOne, locationTwo));
+        assertEquals(0, map.getRoutes(locationOne, locationTwo).size());
+    }
+
+    public void setUpMultiplePaths() {
+//        +-------------------------------+
+//        |                               |
+//        | Approx Locations           7a |
+//        | O = Intersection          X   |
+//        |                          X    |
+//        | From 1a to 7a:          7     |
+//        |                  O4XXX4O      |
+//        | 1-2-7 is fastest 3    26      |
+//        | Then 1-5-6-7     X   X X      |
+//        | Then 1-3-4-7     X  X  6      |
+//        |                  X X  5O      |
+//        |                  32 XX        |
+//        |                  O5X          |
+//        |                 1             |
+//        |                X              |
+//        |               X               |
+//        |              1a               |
+//        |                               |
+//        +-------------------------------+
+
+        this.track1 = new Track(new Point(0, 0), new Point(100, 100));
+        this.track2 = new Track(new Point(100, 100), new Point(200, 200));
+        this.track3 = new Track(new Point(100, 100), new Point(100, 200));
+        this.track4 = new Track(new Point(100, 200), new Point(200, 200));
+        this.track5 = new Track(new Point(100, 100), new Point(200, 150));
+        this.track6 = new Track(new Point(200, 150), new Point(200, 200));
+        this.track7 = new Track(new Point(200, 200), new Point(300, 300));
+
+        map.addTrack(track1);
+        map.addTrack(track2);
+        map.addTrack(track3);
+        map.addTrack(track4);
+        map.addTrack(track5);
+        map.addTrack(track6);
+        map.addTrack(track7);
+
+        this.locationOne = new Location(new Point(0, 0), "location one");
+        this.locationTwo = new Location(new Point(300, 300), "location two");
+
+        map.addLocation(locationOne);
+        map.addLocation(locationTwo);
     }
 
     @Test
     public void testFindRouteMultiplePath() throws Exception {
+        setUpMultiplePaths();
 
+        ArrayList<Track> expectedRoute1 = new ArrayList<Track>();
+        expectedRoute1.add(track1);
+        expectedRoute1.add(track2);
+        expectedRoute1.add(track7);
+
+        ArrayList<Track> expectedRoute2 = new ArrayList<Track>();
+        expectedRoute2.add(track1);
+        expectedRoute2.add(track5);
+        expectedRoute2.add(track6);
+        expectedRoute2.add(track7);
+
+        ArrayList<Track> expectedRoute3 = new ArrayList<Track>();
+        expectedRoute3.add(track1);
+        expectedRoute3.add(track3);
+        expectedRoute3.add(track4);
+        expectedRoute3.add(track7);
+
+        ArrayList<Track> routes = map.getRoutes(locationOne, locationTwo);
+
+        assertTrue(routes.contains(expectedRoute1));
+        assertTrue(routes.contains(expectedRoute2));
+        assertTrue(routes.contains(expectedRoute3));
+        assertEquals(3, routes.size());
     }
 
     @Test
     public void testFindFastestPath() throws Exception {
+        setUpMultiplePaths();
 
+        ArrayList<Track> expectedRoute = new ArrayList<Track>();
+        expectedRoute.add(track1);
+        expectedRoute.add(track2);
+        expectedRoute.add(track7);
+
+        assertEquals(expectedRoute, map.getFastestRoute(locationOne, locationTwo));
     }
 
 }
