c5bf3676fb31e1d279b6d196ad055b1b7bd267b3
Holly Hendry <hrh517@york.ac.uk>
2014-12-03 14:21:58 +0000

    Created InventoryTest file

---
+++ test/lys/sepr/game/resources/InventoryTest.java
@@ -0,0 +1,19 @@
+package lys.sepr.game.resources;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
