2ad34703078dfc25716068d859f9c44af067db71
Holly Hendry <hrh517@york.ac.uk>
2014-12-03 14:22:37 +0000

    Added a collection of variables and methods from the UML

--- src/lys/sepr/game/resources/Resource.java
+++ src/lys/sepr/game/resources/Resource.java
@@ -2,4 +2,55 @@ package lys.sepr.game.resources;
 
 public abstract class Resource {
 
+    public String name;
+    public Integer price;
+    public Integer reqReputation;
+    public Integer maxAllowed;
+    public Integer quantity;
+
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getReqReputation() {
+        return reqReputation;
+    }
+
+    public void setReqReputation(Integer reqReputation) {
+        this.reqReputation = reqReputation;
+    }
+
+    public Integer getMaxAllowed() {
+        return maxAllowed;
+    }
+
+    public void setMaxAllowed(Integer maxAllowed) {
+        this.maxAllowed = maxAllowed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void useResource(Resource resource){
+
+    }
+
 }
