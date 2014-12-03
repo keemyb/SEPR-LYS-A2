2651fe3e3e81e4af69fc94ed02416d841f2b52bf
Holly Hendry <hrh517@york.ac.uk>
2014-12-03 14:22:10 +0000

    Created PowerUpTest file

---
+++ test/lys/sepr/game/resources/PowerUpTest.java
@@ -0,0 +1,13 @@
+package lys.sepr.game.resources;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class PowerUpTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
