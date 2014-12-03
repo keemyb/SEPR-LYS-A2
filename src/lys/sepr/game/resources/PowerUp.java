7acd18afe11dce6b0aa20a3f72b88053ab8003de
Holly Hendry <hrh517@york.ac.uk>
2014-12-03 14:22:52 +0000

    Added a variable to the PowerUp class

--- src/lys/sepr/game/resources/PowerUp.java
+++ src/lys/sepr/game/resources/PowerUp.java
@@ -1,5 +1,9 @@
 package lys.sepr.game.resources;
 
+import java.util.ArrayList;
+
 public class PowerUp extends Resource {
+
+    public ArrayList<PowerUp> allPowerUps = new ArrayList<PowerUp>();
 
 }
