affa2eb15d9410e6226c2ac0ae5cbda7b9fb58c7
Holly Hendry <hrh517@york.ac.uk>
2014-12-03 14:21:29 +0000

    added a variable to the Fuel class

--- src/lys/sepr/game/resources/Fuel.java
+++ src/lys/sepr/game/resources/Fuel.java
@@ -2,4 +2,16 @@ package lys.sepr.game.resources;
 
 public class Fuel extends Resource {
 
+    public Integer value;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+
+
 }
