a41f604c3086790c31f0c3d01fdbe36adcff2e74
Akeem Brown <ab1504@york.ac.uk>
2014-12-20 18:26:59 +0000

    Corrected pickup scaling
    pickup radius should be reduced when zooming in and vice versa

--- src/lys/sepr/mapCreator/MapView.java
+++ src/lys/sepr/mapCreator/MapView.java
@@ -112,7 +112,7 @@ public class MapView {
     private class MouseHandler extends MouseAdapter {
         public void mousePressed(MouseEvent e) {
             lys.sepr.game.world.Point clickPoint = Actions.screenPointToMapPoint(e.getPoint(), state);
-            double scaledPickupDistance = minPickupDistance * state.getZoom();
+            double scaledPickupDistance = minPickupDistance / state.getZoom();
             switch(state.getMode()) {
                 case State.INSPECT_TRACK_MODE:
                     Actions.inspectTrack(map, clickPoint, scaledPickupDistance, state);
