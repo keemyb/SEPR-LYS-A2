800568adaadd9b07e4f210782ac3e622ef0f3817
Akeem Brown <ab1504@york.ac.uk>
2015-01-09 23:20:22 +0000

    Added Contract Class

--- src/lys/sepr/game/Contract.java
+++ src/lys/sepr/game/Contract.java
@@ -1,5 +1,69 @@
 package lys.sepr.game;
 
+import lys.sepr.game.resources.TrainType;
+import lys.sepr.game.world.Location;
+import lys.sepr.game.world.Route;
+import lys.sepr.game.world.Utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Contract {
+
+    private Route initialRoute;
+
+    // Non-mandatory
+    private List<Location> bonusStops = new ArrayList<Location>();
+    private int timeLimit;
+    private TrainType requiredTrainType;
+    private int moneyPayout;
+    private int reputationPayout;
+    private int requiredReputation;
+
+    Contract(Route initialRoute, List<Location> bonusStops, int timeLimit,
+             TrainType requiredTrainType, int moneyPayout,
+             int reputationPayout, int requiredReputation) {
+
+        this.initialRoute = initialRoute;
+        this.bonusStops = bonusStops;
+
+        if (timeLimit < 0) timeLimit = Integer.MAX_VALUE;
+        this.timeLimit = timeLimit;
+
+        this.requiredTrainType = requiredTrainType;
+
+        if (moneyPayout < 0) moneyPayout = (int) Utilities.routeLength(initialRoute.getTracks());
+        this.moneyPayout = moneyPayout;
+
+        if (reputationPayout < 0) reputationPayout = (int) Utilities.routeLength(initialRoute.getTracks());
+        this.reputationPayout = reputationPayout;
+
+        if (requiredReputation < 0) requiredReputation = 0;
+        this.requiredReputation = requiredReputation;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public TrainType getRequiredTrainType() {
+        return requiredTrainType;
+    }
+
+    public int getMoneyPayout() {
+        return moneyPayout;
+    }
+
+    public int getReputationPayout() {
+        return reputationPayout;
+    }
+
+    public int getRequiredReputation() {
+        return requiredReputation;
+    }
+
+    public Route getInitialRoute() {
+        return initialRoute.clone();
+    }
 
 }
--- src/lys/sepr/game/Player.java
+++ src/lys/sepr/game/Player.java
@@ -9,17 +9,17 @@ import java.util.List;
 
 public class Player {
 
-    Contract currentContract;
-    int money;
+    private Contract currentContract;
+    private int money;
 
-    Inventory inventory = new Inventory();
-    ActiveTrain activeTrain;
+    private Inventory inventory = new Inventory();
+    private ActiveTrain activeTrain;
 
-    List<Contract> completedContracts = new ArrayList<Contract>();
-    List<Contract> failedContracts = new ArrayList<Contract>();
+    private List<Contract> completedContracts = new ArrayList<Contract>();
+    private List<Contract> failedContracts = new ArrayList<Contract>();
 
-    int reputation = 0;
-    Profile profile;
+    private int reputation = 0;
+    private Profile profile;
 
     Player(int money) {
         this.money = money;
@@ -31,10 +31,15 @@ public class Player {
 
     public void completedCurrentContract() {
         if (currentContract != null) completedContracts.add(currentContract);
+        addMoney(currentContract.getMoneyPayout());
+        updateReputation(currentContract.getReputationPayout());
+        endTurn();
     }
 
     public void failedCurrentContract() {
         if (currentContract != null) failedContracts.add(currentContract);
+        updateReputation(-(currentContract.getReputationPayout()) / 2);
+        endTurn();
     }
 
     public Inventory getInventory() {
@@ -42,10 +47,39 @@ public class Player {
     }
 
     public void beginTurn(Train train, Contract contract) {
-        activeTrain = new ActiveTrain(train, contract.getRoute());
+        activeTrain = new ActiveTrain(train, contract.getInitialRoute());
     }
 
-    public void endTurn() {
+    private void endTurn() {
         activeTrain = null;
+    }
+
+    public void addMoney(int money) {
+        if (money < 0) return;
+        this.money += money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public boolean spendMoney(int money) {
+        //Successful spend returns true.
+
+        if (money < 0) return false;
+
+        if (this.money < money) return false;
+
+        this.money -= money;
+        return true;
+    }
+
+    public void updateReputation(int amount) {
+        reputation += amount;
+        if (reputation < 0) reputation = 0;
+    }
+
+    public int getReputation() {
+        return reputation;
     }
 }
