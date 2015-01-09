0133363aa727df7528ff2acfef4514f8ca24283f
Akeem Brown <ab1504@york.ac.uk>
2015-01-09 01:05:56 +0000

    Created ActiveTrain Constructor
    No longer extends Train, uses composition.

--- src/lys/sepr/game/ActiveTrain.java
+++ src/lys/sepr/game/ActiveTrain.java
@@ -1,7 +1,40 @@
 package lys.sepr.game;
 
 import lys.sepr.game.resources.Train;
+import lys.sepr.game.world.Point;
+import lys.sepr.game.world.Route;
+import lys.sepr.game.world.Track;
+import lys.sepr.game.world.Utilities;
 
-public class ActiveTrain extends Train {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ActiveTrain {
+
+    private Train train;
+    private Point currentPosition;
+    private Point destination;
+    private Point facing;
+    private List<Track> remainderOfRoute = new ArrayList<Track>();
+    private Double orientation;
+    private Double currentSpeed;
+
+    public ActiveTrain(Train train, Route initialRoute) {
+        this.train = train;
+        remainderOfRoute = initialRoute.getTracks();
+        Track firstTrack = remainderOfRoute.get(0);
+
+        currentPosition = Utilities.closestPoint(initialRoute.getFrom(), firstTrack);
+        destination = initialRoute.getTo();
+
+        if (remainderOfRoute.size() > 1) {
+            Track secondTrack = remainderOfRoute.get(1);
+            facing = firstTrack.getCommonPoint(secondTrack);
+        } else {
+            facing = Utilities.closestPoint(destination, firstTrack);
+        }
+
+        orientation = Math.toDegrees(Math.atan2(facing.getY() - currentPosition.getY(),
+                facing.getX() - currentPosition.getX()));
+    }
 }
--- src/lys/sepr/game/resources/Train.java
+++ src/lys/sepr/game/resources/Train.java
@@ -141,7 +141,7 @@ public class Train extends Resource{
 
 
 
-    Train(String name, int price, int reqReputation, int maxAllowed) {
+    public Train(String name, int price, int reqReputation, int maxAllowed) {
         super(name, price, reqReputation, maxAllowed);
     }
 }
