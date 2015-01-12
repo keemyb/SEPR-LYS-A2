fbc756556054525aaf3550917498948efb76b762
Arushi Aneja <aa1249@york.ac.uk>
2015-01-12 16:13:18 +0000

    Changed the maximum capacity of the inventory from 12 to 7 as mentioned in the brief.

--- src/lys/sepr/game/resources/Inventory.java
+++ src/lys/sepr/game/resources/Inventory.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Inventory {
 //trains, power-ups, fuel
 
-    private static final int MAX_CAPACITY = 12;
+    private static final int MAX_CAPACITY = 7;
     private ArrayList<Resource> contents= new ArrayList<Resource>();
 
     public ArrayList<Resource> getContents() {
